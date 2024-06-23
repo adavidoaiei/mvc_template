8eba0d6c585701e6aa96b7e2c7e7737c39b5bd1b
adavidoaiei <cornel.adavidoaiei@gmail.com>
2024-06-23 13:01:50 +0300

    build

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ad1fcacdd3dd221e89dcfa769aeb8133a3845e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3c900027b25bf0da362e190f1a038e031d1eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
