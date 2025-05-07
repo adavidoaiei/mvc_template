7443d1f9a8b0107b76d9f36c936a0aa179a9098f
adavidoaiei <cornel.adavidoaiei@gmail.com>
2025-05-07 23:04:02 +0300

    Update assembly information and regenerate binaries

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e947f864c7462904f2cea48f494cb3bef61bde37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc7ab66714a3acb31b7cc99476e2f7599e0bdf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
