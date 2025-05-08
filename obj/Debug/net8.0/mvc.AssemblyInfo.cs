e471f1cd36fe6cb564864263c7141a518f0d4cab
adavidoaiei <cornel.adavidoaiei@gmail.com>
2025-05-08 21:11:41 +0300

    Update assembly version and source link for debugging

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7443d1f9a8b0107b76d9f36c936a0aa179a9098f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b79fd19ba75230723ac18190370fc70128b402")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
